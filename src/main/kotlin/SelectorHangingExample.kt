27970875c1369a890cfb8093220dcbc4e5de5b95
grigorov <grigorov@vologda.softlab.ru>
2018-10-12 11:21:11 +0300

    1. Пример переведён на работу с non-persistent очередями. 2. Испрвлена ошибка с открытием/закрытием jms-соединения.

--- src/main/kotlin/SelectorHangingExample.kt
+++ src/main/kotlin/SelectorHangingExample.kt
@@ -1,41 +1,32 @@
 import org.apache.activemq.ActiveMQConnectionFactory
 import javax.jms.Session
 
-fun main(args: Array<String>) {
-    val cf = ActiveMQConnectionFactory("vm://localhost?broker.persistent=true")
+const val MESSAGE_COUNT: Int = 200; // Значение 200 выбрано, потому что pageSize по умолчанию равно 200.
 
-    var conn = cf.createConnection();
+fun main(args: Array<String>) {
+    val cf = ActiveMQConnectionFactory("vm://localhost?broker.persistent=false")
+    val conn = cf.createConnection();
     try {
         conn.start();
         val session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
         try {
             val queue = session.createQueue("dynamicQueues/test")
             val producer = session.createProducer(queue);
-            for (i in 1..200) {
+
+            for (i in 1..MESSAGE_COUNT) {
                 val message = session.createTextMessage("Message A #$i");
                 message.setStringProperty("type", "A");
                 producer.send(message)
             }
-            for (i in 1..200) {
+
+            for (i in 1..MESSAGE_COUNT) {
                 val message = session.createTextMessage("Message B #$i");
                 message.setStringProperty("type", "B");
                 producer.send(message)
             }
-        } finally {
-            session.close();
-        }
-    } finally {
-        conn.close()
-    }
 
-    conn = cf.createConnection();
-    try {
-        val session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        try {
-            val queue = session.createQueue("dynamicQueues/test")
             val consumer = session.createConsumer(queue, "type='B'");
             consumer.setMessageListener { m -> println(m) }
-            conn.start();
             Thread.sleep(5000)
         } finally {
             session.close();
